280a17534c5526a802b5341252f22d5bb1b98eea
Ari <127914863+isaAJ05@users.noreply.github.com>
2026-07-19 00:22:59 -0500

    w03 home page project and optimize files

--- scripts/hamburguer.js
+++ scripts/hamburguer.js
@@ -1,9 +1,7 @@
 const hamButton = document.querySelector('#menu');
 const navigation = document.querySelector('.navigation');
-const title = document.querySelector('.title');
 
 hamButton.addEventListener('click', () => {
 	navigation.classList.toggle('open');
     hamButton.classList.toggle('open');
-    title.classList.toggle('open');
 });
--- scripts/weather.js
+++ scripts/weather.js
@@ -1,18 +1,18 @@
 const currentTemp = document.querySelector("#current-temp");
 const weatherIcon = document.querySelector("#weather-icon"); 
-const captionDesc = document.querySelector("figcaption"); 
+const captionDesc = document.querySelector("#current-desc");
 
-const myKey = "";
-const myLat = "49.7602869991973";
-const myLong = "6.6462879584165";
-const url = `https://api.openweathermap.org/data/2.5/weather/?lat=${myLat}&lon=${myLong}&units=metric&lang=en&appid=${myKey}`;
+const myKey = "6c5aa4ac22c2ca89e974d989956ba6dc";
+const myLat = "10.96854";
+const myLong = "-74.78132";
+const url2 = `https://api.openweathermap.org/data/2.5/weather/?lat=${myLat}&lon=${myLong}&units=metric&lang=en&appid=${myKey}`;
 async function apiFetch() {
   try {
-    const response = await fetch(url);
+    const response = await fetch(url2);
     if (response.ok) {
       const data = await response.json();
       console.log(data); // testing only
-      displayResults(data); // uncomment when ready
+      displayResults(data); 
     } else {
         throw Error(await response.text());
     }
@@ -23,10 +23,10 @@ async function apiFetch() {
 apiFetch();
 
 function displayResults(data) {
-  currentTemp.innerHTML = `${data.main.temp}&deg;F`;
+  currentTemp.innerHTML = `${data.main.temp}&deg;C`;
   const iconsrc = `https://openweathermap.org/img/wn/${data.weather[0].icon}@2x.png`;
   let desc = data.weather[0].description;
   weatherIcon.setAttribute('src', iconsrc);
   weatherIcon.setAttribute('alt', desc);
-  captionDesc.textContent = `${desc}`;
+  captionDesc.innerHTML = `${desc}`;
 }
