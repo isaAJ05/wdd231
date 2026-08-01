8fcb6204572bcbcf86284e5b48255647e70bbe39
Ari <127914863+isaAJ05@users.noreply.github.com>
2026-08-01 17:43:27 -0500

    Join Page - Chamber

--- chamber/scripts/join.js
+++ chamber/scripts/join.js
@@ -1,6 +1,25 @@
-const timestamp = document.querySelector("#timestamp");
+const campo = document.querySelector("#organizationTitle");
+const regex = /^[-A-Za-z ]{7,}$/;
 
-timestamp.value = new Date().toISOString();
+campo.addEventListener("input", () => {
+    if (campo.value === "") {
+        campo.setCustomValidity(""); // no error if empty
+    } else if (regex.test(campo.value)) {
+        campo.setCustomValidity(""); // clears errors
+    } else {
+        campo.setCustomValidity("Must contain at least 7 characters, only letters, spaces, and hyphens");
+    }
+    });
+
+
+
+const timestamp = document.querySelector("#timestamp");
+const now = new Date();
+timestamp.value = now
+  .toISOString()
+  .replace("T", " ")
+  .replace("Z", "")
+  .split(".")[0];
 
 const links = document.querySelectorAll("[data-modal]");
 
